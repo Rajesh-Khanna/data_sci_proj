94bbf1559f40a47fb0706da1c6cfc8d1b430c11e
Rajesh-Khanna <chrajeshkhanna82798@gmail.com>
2017-12-09 11:40:16 +0530

    modified

--- data_classification.py
+++ data_classification.py
@@ -1,15 +1,207 @@
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import MiniBatchKMeans,KMeans,SpectralClustering,MeanShift,AffinityPropagation,AgglomerativeClustering,DBSCAN,Birch
+from sklearn.mixture import GaussianMixture
 import numpy as np
 import data_modification as dm
-
+import pywt
+import pandas as pd
+import matplotlib.pyplot as plt
 trls = 168
 duration = 300
-
-dc = dm.ex_d('data_set_IVa_aa_cnt.txt','data_set_IVa_aa_mrk.txt',trls,duration)
-dp= dc.MEAN()
-mkm = MiniBatchKMeans(n_clusters=2)
-mkm.fit(dp)
-labels = mkm.labels_
+reduction = 0
+sub_sets = 1
+dc = dm.ex_d('data_set_IVa_aa_cnt.txt','data_set_IVa_aa_mrk.txt',trls,duration,reduction,sub_sets)
+dpz =[]
+dpz.append(dc.MEAN())
+dpz.append(dc.MEDIAN())
+dpz.append(dc.VARIANCE())
+dpz.append(dc.coeff_var())
+dpz.append(dc.wave_let())
+#p = pd.DataFrame({'test':dc[0][0].tolist()})
 targets = dc.labels
-for i in range(trls):
-	print(labels[i]-targets[i]+1,end='')
+for g in range(10):
+	x = np.array(range(118))
+	y = np.zeros((118))
+	j=0
+	for i in dpz[0][g]:
+		y[j]+=(i)
+		j+=1
+	plt.plot(x, y)
+	plt.title(targets[g])
+	plt.show()
+
+max = 0
+
+for dp in dpz:
+	print('\n\n\n****************     *******************\n\n\n')
+	mkm = MiniBatchKMeans(n_clusters=2)
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if((labels[i] == targets[i]) or (labels[i] + 2 == targets[i])):
+				count+=1
+
+	print('MiniBatchKMeans : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%')
+	print('')
+
+	mkm = KMeans(n_clusters=2)
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+
+	print('KMeans : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+	mkm = SpectralClustering(n_clusters=2)
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+
+	print('SpectralClustering : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+	mkm = MeanShift()
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+	print('MeanShift : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+	mkm = AffinityPropagation()
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+	print('AffinityPropagation : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+	
+
+	mkm = AgglomerativeClustering(n_clusters =2)
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+	print('AgglomerativeClustering : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+
+	mkm = DBSCAN()
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+	print('DBSCAN : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+	mkm = Birch()
+	mkm.fit(dp)
+	labels = mkm.labels_
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+	print('Birch : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+
+	mkm = GaussianMixture()
+	mkm.fit(dp)
+	labels = mkm.predict(dp)
+	count = 0
+	for i in range(trls):
+		if(labels[0]==0 and labels[1]==0):
+			if(labels[i]+1 == targets[i]):
+				count+=1
+		else:
+			if(labels[i] == targets[i] or labels[i] + 2 == targets[i]):
+				count+=1
+	print('GaussianMixture : ',end='')
+	acuracy = count*100/trls
+	if(max<acuracy):
+		max = acuracy
+	print(acuracy,end='')
+	print('%\n')
+
+
+print('best accuracy achieved : ',end='')
+print(max)
